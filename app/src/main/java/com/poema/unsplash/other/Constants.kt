00666b4ed5fe4db5a9f62fceeef6e98fd392cbdc
Andreas Svedstedt <andreas@poema.se>
2021-12-23 21:10:50 +0100

    la till någon constant

---
+++ app/src/main/java/com/poema/unsplash/other/Constants.kt
@@ -0,0 +1,6 @@
+package com.poema.unsplash.other
+
+object Constants {
+    const val CHARS_BEFORE_CUTTING = 48
+    const val WHERE_TO_STOP_LOOKING_FOR_A_SPACE = 30
+}
--- app/src/main/java/com/poema/unsplash/ui/uimodel/Photo.kt
+++ app/src/main/java/com/poema/unsplash/ui/uimodel/Photo.kt
@@ -6,6 +6,8 @@ import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.poema.unsplash.R
+import com.poema.unsplash.other.Constants.CHARS_BEFORE_CUTTING
+import com.poema.unsplash.other.Constants.WHERE_TO_STOP_LOOKING_FOR_A_SPACE
 
 @BindingAdapter("android:glideImages")
 fun glideImages(imageView: ImageView, item: Photo) {
@@ -24,10 +26,10 @@ fun fixDescription(textView: TextView, item: Photo) {
     val ch = ' '
     var lastString = ""
     item.description?.let {
-        if (item.description!!.length > 48
+        if (item.description!!.length > CHARS_BEFORE_CUTTING
         ) {
-            processingString  = item.description!!.subSequence(0, 47).toString()
-            for( index in processingString.length-1 downTo 30 ) {
+            processingString  = item.description!!.subSequence(0, CHARS_BEFORE_CUTTING-1).toString()
+            for( index in processingString.length-1 downTo WHERE_TO_STOP_LOOKING_FOR_A_SPACE ) {
                 if (processingString[index] == ch){
                     lastString = processingString.subSequence(0, index).toString()
                     break
